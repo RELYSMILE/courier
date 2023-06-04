f3d14c848d95d3e72774d287453c67d42a9ab8c1
RELYSMILE <relysmiled@gmail.com>
2023-06-04 18:21:28 +0100

    railway

--- consignment/settings.py
+++ consignment/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
